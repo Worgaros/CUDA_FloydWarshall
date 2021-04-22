3c228ba12813235e4a14d69d92d9598aa7b5bd49
William <williamp10022000@gmail.com>
2021-04-22 14:25:06 +0200

    applying google coding style and trying to show the path and trying to fix random generator

--- src/floyd_warshall.cu
+++ src/floyd_warshall.cu
@@ -1,121 +1,97 @@
-#include "floyd_warshall.cuh"
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include <limits.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
-#include <device_launch_parameters.h>
-#include <vector>
 #include <cuda_runtime_api.h>
+#include <device_launch_parameters.h>
+#include <iostream>
 #include <memory>
 
+#include "floyd_warshall.cuh"
 
-#define min(a,b) (((a)<(b))?(a):(b))
-#define BLOCK_SIZE 16
-
-static size_t data_size = 10;
-
-//state variable
-static uint64_t rand_state;
-
-__global__ void parallel_floyd_warshall(int* graph, int n, int* path)
-{
+__global__ void ParallelFloydWarshall(int* graph, int n, int* path) {
+	const int block_size = 16;
 	// block indices
-	int bx = blockIdx.x;
-	int by = blockIdx.y;
-	int tx = threadIdx.x;
-	int ty = threadIdx.y;
+	const int bx = blockIdx.x;
+	const int by = blockIdx.y;
+	const int tx = threadIdx.x;
+	const int ty = threadIdx.y;
 
-	int aBegin = n * BLOCK_SIZE * by;
-	int aEnd = aBegin + n - 1;
-	int aStep = BLOCK_SIZE;
+	const int a_begin = n * block_size * by;
+	const int a_end = a_begin + n - 1;
+	const int a_step = block_size;
 
-	int bBegin = BLOCK_SIZE * bx;
-	int bStep = BLOCK_SIZE * n;
+	const int b_begin = block_size * bx;
+	const int b_step = block_size * n;
 
 	int pathsub = 0;
 
-	for (int a = aBegin, b = bBegin; a <= aEnd; a += aStep, b += bStep) {
-		//load block into shared memory
-		__shared__ int graph_s[BLOCK_SIZE][BLOCK_SIZE];
-		__shared__ int path_s[BLOCK_SIZE][BLOCK_SIZE];
+	for (int a = a_begin, b = b_begin; a <= a_end; a += a_step, b += b_step) {
+		// Load block into shared memory.
+		int graph_s[block_size][block_size];
+		int path_s[block_size][block_size];
 		graph_s[ty][tx] = graph[a + n * ty + tx];
 		path_s[ty][tx] = path[b + n * ty + tx];
 		__syncthreads();
 
-		//find minimum for block
-		for (int k = 0; k < BLOCK_SIZE; ++k) {
+		// Find minimum for block.
+		for (int k = 0; k < block_size; ++k) {
 			pathsub = graph_s[ty][k] < graph_s[ty][k] + path_s[k][tx] ?
 				graph_s[ty][k] : graph_s[ty][k] + path_s[k][tx];
 		}
 		__syncthreads();
 	}
-	//writeback
-	int pathwrite = n * BLOCK_SIZE * by + BLOCK_SIZE * bx;
+	// Writeback.
+	int pathwrite = n * block_size * by + block_size * bx;
 	path[pathwrite + n * ty + tx] = pathsub;
-
 }
 
 
 int main()
 {
-	//create graph
-	//int* graph = (int*)calloc(sizeof(int), n * n);
-	//for (int i = 0; i < n; i++) {
-	//	for (int j = 0; j < n; j++) {
-	//		if (i == j) {
-	//			graph[(i * n) + j] = 0;
-	//		}
-	//		else {
-	//			graph[(i * n) + j] = xrand();
-	//		}
-	//	}
-	//}
+	// Create graph density will be between 0 and 100,
+	// indication the % of number of directed edges in graph
+	// range will be the range of edge weighting of directed edges.
 
-	//density will be between 0 and 100, indication the % of number of directed edges in graph
-	//range will be the range of edge weighting of directed edges
-	
-		int N = 700;
-		int density = 25;
-		int Prange = (100 / density);
-		int* G = (int*)calloc(sizeof(int), N * N);
-		int range = 1000;
-		int INF = std::numeric_limits<int>::max();
-			for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (i == j) {//set G[i][i]=0
-					G[i * N + j] = 0;
-					continue;
-				}
-				int pr = rand() % Prange;
-				G[i * N + j] = pr == 0 ? ((rand() % range) + 1) : INF;
-				//set edge random edge weight to random value, or to INF
+	const int n = 700;
+	const int density = 25;
+	const int prange = (100 / density);
+	int* graph = (int*)calloc(sizeof(int), n * n);
+	const int range = 1000;
+	const int infinite = std::numeric_limits<int>::max();
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			// Set G[i][i] = 0.
+			if (i == j) {
+				graph[i * n + j] = 0;
+				continue;
 			}
+			int pr = std::rand() % prange;
+			// Set edge random edge weight to random value, or to infinite.
+			graph[i * n + j] = pr == 0 ? ((rand() % range) + 1) : infinite;
 		}
-	
+	}
 
-	int* path = NULL;
-
+	int* path = nullptr;
 	
 	int* graph_d;
-	cudaMalloc(&graph_d, N * N);
-	cudaMemcpy(graph_d, G, N * N, cudaMemcpyHostToDevice);
+	cudaMalloc(&graph_d, n * n);
+	cudaMemcpy(graph_d, graph, n * n, cudaMemcpyHostToDevice);
 
 	int* path_d;
-	cudaMalloc(&path_d, N * N);
+	cudaMalloc(&path_d, n * n);
 
-	int grid = 1;
-	int block = 1;
+	const int grid = 1;
+	const int block = 1;
 
-	parallel_floyd_warshall <<<grid, block >>> (graph_d, N, path_d);
+	ParallelFloydWarshall <<<grid, block>>> (graph_d, n, path_d);
 
+	cudaMemcpy(path, path_d, n * n, cudaMemcpyDeviceToHost);
 
-	//free memory
+	std::cout << path;
+
+	// Free memory.
 	cudaFree(path_d);
 	cudaFree(graph_d);
 	free(path);
-	free(G);
-	return 0;
+	free(graph);
+	return EXIT_SUCCESS;
 }
--- src/floyd_warshall.cuh
+++ src/floyd_warshall.cuh
@@ -1,23 +1,6 @@
-﻿#ifndef __PARALLEL_FLOYD_WARSHALL_H__
-#define __PARALLEL_FLOYD_WARSHALL_H__
+﻿#pragma once
 
-#include <stdint.h>
 #include <cuda.h>
-#include <cuda_runtime.h>
-#include <device_launch_parameters.h>
+#include <cuda_runtime.h>/
 
-
-//timing functions
-struct timespec timer_start();
-long int timer_end(struct timespec start_time);
-
-//xorshift* algorithm for PRNG
-uint64_t xorshift(void);
-uint64_t xrand(void);
-void xseed(uint64_t seed);
-
-//floyd-Warshall functions
 __global__ void parallel_floyd_warshall(int* graph, int n, int* path);
-int* serial_floyd_warshall(int* graph, int n);
-
-#endif
